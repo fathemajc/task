1a807b0af5e08981c13649da0b3888b393308352
Shahrin <cse_2012020197@lus.ac.bd>
2021-10-06 01:31:02 -1200

    first commit

---
+++ JavaClass.java
@@ -0,0 +1,18 @@
+ 
+public class  JavaClass{
+   /*
+Name : Fatema Jannat chowdhury
+ID : 2012020197
+Section :  D
+Email: cse_2012020197@lu.ac.bd
+Date: 07-08-21
+ */
+     public static void main(String[] args) {
+        Student stud1 = new Student();
+        Student stud2 = new Student(2012020197,"Fatema.");
+        Student stud3 = new Student(2000001000,"Amina.");
+        stud1.display();
+        stud2.display();
+        stud3.display();
+    }
+}
